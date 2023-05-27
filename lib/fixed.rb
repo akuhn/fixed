8b50599eb44a06d7797cd8176ce18fcfb0aa10cd
Adrian Kuhn <akuhn@iam.unibe.ch>
2023-05-28 00:43:28 +0100

    :sunrise:

---
+++ lib/fixed.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "fixed/version"
+
+module Fixed
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/fixed_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Fixed do
+  it "has a version number" do
+    expect(Fixed::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
